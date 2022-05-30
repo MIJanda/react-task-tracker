88dd657e81a617d01680153912705eda83f37e2f
MIJanda <mokiliisaacjanda@gmail.com>
2022-05-30 06:22:01 +0300

    add state to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import {useState} from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
+    const [tasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors appointment',
+            day: 'Jun 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Daily standup',
+            day: 'Jun 7th at 5:30pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Grocery shopping',
+            day: 'Jun 10th at 10:30am',
+            reminder: false,
+        },
+    ])
+
 	return (
 		<div className="container">
 			<Header/>
+            <Tasks tasks={tasks}/>
 		</div>
 	);
 }
