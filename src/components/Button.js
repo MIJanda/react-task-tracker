15beb515669367f19f383b14aa5ac25179171102
MIJanda <mokiliisaacjanda@gmail.com>
2022-05-30 05:42:20 +0300

    add and embed onClick event to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
+const Button = ({color, text, onClick}) => {
   return <button 
+            onClick = {onClick}
             style = {{ backgroundColor: color}} 
-            className="btn">{text}
+            className="btn"
+        >
+            {text}
         </button>
 }
 
 Button.defaultProps = {
     color: 'steelblue',
+    text: 'Add',
 }
 
 Button.propTypes = {
